874530bc805722acf545cab997cabdc5e0b0af94
Dhanya  Nagaraj <115769407+Dhanyanagaraj@users.noreply.github.com>
2022-12-08 14:34:51 +0000

    completed 09

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,17 +1,11 @@
-  #include <stdio.h>
-  #include <math.h>
-  int main()
-    {
-    double n, x, y;
-     printf("Enter a number: ");
-     scanf("%lf", &n);
-       x = n;
-      y = 1;
-      while (x - y > 0.000001)
-        {
-          x = (x + y)/2;
-          y = n/x;
-        }
-      printf("Square root of %lf = %lf", n, x);
-        return 0;
-    }
+ #include <stdio.h>
+#include <math.h>
+int main()
+{
+    double num, root;
+    printf("Enter any number to find square root: ");
+    scanf("%lf", &num);
+    root = sqrt(num);
+    printf("Square root of %.2lf = %.2lf", num, root);
+    return 0;
+}
