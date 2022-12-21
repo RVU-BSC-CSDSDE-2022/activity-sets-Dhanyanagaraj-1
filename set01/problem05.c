ff4cf9b011b07d92689acc1f8abe798caa3f3cd5
Dhanya  Nagaraj <115769407+Dhanyanagaraj@users.noreply.github.com>
2022-12-21 08:20:49 +0000

    completed 05

--- set01/problem05.c
+++ set01/problem05.c
@@ -3,34 +3,3 @@ int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
 
-int main()
-{
-  int a,b,c,largest;
-  printf("Enter the number");
-  a=input();
-  b=input();
-  c=input();
-  largest=compare(a,b,c);
-  output(a,b,c,largest);
-  return 0; 
-}
-
-int input()
-{
-  int n;
-  if(scanf("%d",&n));
-  return n;
-}
-
-int compare(int a, int b, int c)
-{
-  if(a>b && a>c)
-    return a;
-  if(b>c)
-    return b;
-  return c;
-}
-void output(int a, int b, int c, int largest)
-{
-  printf("The largrest of %d,%d,%d is %d",a,b,c,largest)
-}
