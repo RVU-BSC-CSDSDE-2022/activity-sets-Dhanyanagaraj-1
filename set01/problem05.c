4a804531465fda714ee068b52586518b748d52b1
Dhanya  Nagaraj <115769407+Dhanyanagaraj@users.noreply.github.com>
2023-01-05 16:12:48 +0000

    05

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,19 +1,44 @@
-  #include <stdio.h>
+ #include <stdio.h>
 int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
 
-int input()
-{
-  int n;
-  printf("enter a number\n");
-  scanf("&d",&n)
-    return n;
+int main() {
+  int a, b, c;
+  int largest;
+  a = input();
+  b = input();
+  c = input();
+  largest = compare(a, b, c);
+  output(a, b, c, largest);
+  return 0;
 }
 
-int find_largest(int a,int b,int c,);
-{
-  printf("enter the number %d\n");
+int input() {
+  int n;
+  printf("enter the 3 nos");
+  scanf("%d", &n);
+  return n;
 }
+
+int compare(int a, int b, int c) {
+  int largest; 
+  {
+    if (a >= b && a >= c) {
+      largest = a;
+    } else if (b >= a && b >= c) {
+      largest = b;
+    } else {
+      largest = c;
+    }
+  }
+  return largest;
+}
+
+void output(int a, int b, int c, int largest) {
+  printf(" the largest number is %d", largest);
+}
+
+
 
 
