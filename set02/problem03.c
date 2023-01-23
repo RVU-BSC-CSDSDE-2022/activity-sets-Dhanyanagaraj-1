f366d78035acfeb6ab40d33722c3fcd8dfc2d748
Dhanya  Nagaraj <115769407+Dhanyanagaraj@users.noreply.github.com>
2023-01-23 09:54:07 +0000

    completed 03

--- set02/problem03.c
+++ set02/problem03.c
@@ -1,41 +1,37 @@
-#include <stdio.h>
+#include<stdio.h>
 int input_number();
 int is_composite(int n);
 void output(int n, int result);
-
 int main()
 {
-  int n ,result;
+  int n,result;
   n=input_number();
-  result=is_composite(n);
+  result=is_compsite(n);
   output(n,result);
   return 0;
 }
-
 int input_number()
 {
   int n;
-  printf("Enter the number:");
-  if(scanf("%d",&n));
+  printf("enter the number:");
+  scanf("%d",&n);
   return n;
 }
-
 int is_composite(int n)
 {
-  for (int i=2 ; i<n ; i++)
+  for( i=2;i<n;i++)
     {
-      if(n%i == 0)
+      if(n%i==0)
       {
         return 1;
       }
     }
-    return 0;
+  return 0;
 }
-
 void output(int n, int result)
 {
   if(result==1)
-    printf("%d is a Composite number.\n",n);
+    printf("%d is a compsite number\n",n);
   else
-    printf("%d is not a Composite number.\n",n);
-} 
+    printf("%d is not a compsite number\n",n);
+ } 
