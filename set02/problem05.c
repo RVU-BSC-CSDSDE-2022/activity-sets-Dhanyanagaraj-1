64bc232ec243858138d22b0c24176c919981641a
Dhanya  Nagaraj <115769407+Dhanyanagaraj@users.noreply.github.com>
2023-01-23 14:24:46 +0000

    completed 05

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,38 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
 int input();
 int find_gcd(int a, int b);
 void output(int a, int b, int gcd);
-
 int main()
 {
-  int a,b,gcd;
-  a=input();
-  a=input();
-  gcd=find_gcd(a,b);
-  output(a,b,gcd);
-  return 0;
+  int a,b,gdc;
+  a= input();
+  b=input();
+  gdc=find_gdc(a,b);
+  output(a,b,gdc);
 }
-
-int input();
+int input()
 {
   int n;
-  printf("Enter the numbers: ");
-  if(scanf("%d",&n));
+  printf("enter the number:");
+  scanf("%d",&n);
   return n;
 }
-
 int find_gcd(int a, int b)
 {
   int gcd;
-  for( int i=1 ; i<=a && i<=b ; i++)
+  for(int i=0;i<=a && i<=b;i++)
     {
-      if(a%i==0 && b%i==0)
-        gcd = i;
+      if(a%i==0  && b%i==0)
+        gcd=i;
     }
   return gcd;
-} 
-
+}
 void output(int a, int b, int gcd)
 {
   printf("G.C.D of %d and %d is %d\n",a,b,gcd);
-} 
+}
